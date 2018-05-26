4eca0377486609bf1528fe69f0467301f31677a6
Ralf Luther <luther.ralf@gmail.com>
2018-05-26 18:01:41 +0200

    msm8998: Use Google full APN list
    Use apns-full-conf.xml from Google Marlin (2018)
    as Lineage APN updates are (still) pending
    
    Change-Id: I276d8d957c4195d7be3cd4f00338de1c8c593c26

--- common.mk
+++ common.mk
@@ -399,6 +399,10 @@ PRODUCT_BOOT_JARS += \
     qcrilhook \
     telephony-ext
 
+# List Of APNS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/common/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
